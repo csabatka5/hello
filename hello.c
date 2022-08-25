7900a8a7106086b618d992b0f2ed6452f68d3908
csabatka5 <111770954+csabatka5@users.noreply.github.com>
2022-08-25 20:37:11 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Chloe Sabatka
+ * csabatka@unomaha.edu
+ * 2022/08/25
+ *
+ * Hack 1
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Chloe Sabatka!\n");
+  printf("My major is: Electrical Engineering.\n");
+
+  return 0;
+}
